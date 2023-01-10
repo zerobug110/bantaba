7a34860fa10231cdd4783f729a60ae509f0ecbd4
zerobug110 <mahabojang110@gmail.com>
2023-01-10 16:07:07 +0000

    added sass and setup starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import './App.css';
-import { Form } from './components/form';
-import { Home } from './components/home';
-// import {Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <Routes>
-          <Route>
-
-          </Route>
-
-        </Routes>
-      </Router> */}
-      <Form/>
-      <Home/>
+      <h1>hello</h1>
     </div>
   );
 }
