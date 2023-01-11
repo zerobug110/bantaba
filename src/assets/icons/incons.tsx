aebd015d9eb023b9101c025348cdf34ed2d771e0
zerobug110 <mahabojang110@gmail.com>
2023-01-11 04:15:55 +0000

    fixed the react router dom and added svg icons

---
+++ src/assets/icons/incons.tsx
@@ -0,0 +1,59 @@
+
+export const CloseIcon = () => {
+    return(
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32"
+      >
+        <path d="M24 20v-4H14v-4h10V8l6 6zm-2-2v8H12v6L0 26V0h22v10h-2V2H4l8 4v18h8v-6z"></path>
+      </svg>
+    )
+}
+
+export const HomeIcon = () => {
+    return(
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32"
+      >
+        <path d="M32 19l-6-6V4h-4v5l-6-6L0 19v1h4v10h10v-6h4v6h10V20h4z"></path>
+      </svg>
+    )
+}
+
+export const SendIcon = () => {
+    return(
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M2.016 21v-6.984l15-2.016-15-2.016V3l21 9z"></path>
+      </svg>
+    )
+}
+
+export const LikeIcon = () => {
+    return(
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+      >
+        <path d="M24 20v-4H14v-4h10V8l6 6zm-2-2v8H12v6L0 26V0h22v10h-2V2H4l8 4v18h8v-6z"></path>
+      </svg>
+    )
+}
+
+export const AddIcon = () => {
+    return(
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+      >
+        <path d="M24 20v-4H14v-4h10V8l6 6zm-2-2v8H12v6L0 26V0h22v10h-2V2H4l8 4v18h8v-6z"></path>
+      </svg>
+    )
+}
