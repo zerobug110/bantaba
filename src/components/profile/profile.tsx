ad82e88a7f68057b475ed1df50756bda8b59421c
zerobug110 <mahabojang110@gmail.com>
2023-01-12 01:55:41 +0000

    added new image

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -6,7 +6,7 @@ export const Profile = () => {
         <Card >
             <div className="profile">
                 <div className="profile__background">
-                    
+                    <img src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80" alt="" className="img" />
                 </div>
                 <div className="profile__info">
                   <div className="profile__info--primary">
@@ -17,7 +17,7 @@ export const Profile = () => {
                     </div>
 
                     <div className="profile-image">
-                        <img src="" alt="" className="img" />
+                        <img src="https://images.unsplash.com/photo-1614274300320-f8d5002cf6e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fGJsYWNrJTIwbW9kZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className="img" />
                     </div>
 
                     <div className="following">
@@ -25,7 +25,6 @@ export const Profile = () => {
                         <div className="following__label">following</div>
 
                     </div>
-
                   </div>
                 </div>
             </div>
