08d2f903b6ab6e45fb72c1838f31494136686701
zerobug110 <mahabojang110@gmail.com>
2023-01-14 04:00:28 +0000

    changed the grid layout of home component

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -136,3 +136,75 @@ export const ChatIcon = () => {
     </svg>
   )
 }
+
+export const ImageIcon = () => {
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M13.5 6c-.8 0-1.5.7-1.5 1.5S12.7 9 13.5 9 15 8.3 15 7.5 14.3 6 13.5 6zM19 2H5C3.3 2 2 3.3 2 5v14c0 1.7 1.3 3 3 3h14c1.7 0 3-1.3 3-3V5c0-1.7-1.3-3-3-3zm1 11.9L18.1 12c-1.2-1.1-3.1-1.1-4.2 0l-.9.9-2.9-2.9C8.9 8.9 7 8.9 5.9 10L4 11.9V5c0-.6.4-1 1-1h14c.6 0 1 .4 1 1v8.9z"></path>
+  </svg>
+  )
+}
+
+export const VideoIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+      <path
+        fill="#222"
+        d="M51.83 8H12.17a10 10 0 00-10 10v28a10 10 0 0010 10h39.66a10 10 0 0010-10V18a10 10 0 00-10-10zm-9.12 26.64l-16.41 8.8a3 3 0 01-1.42.36 3 3 0 01-3-3V23.2a3 3 0 014.42-2.64l16.41 8.8a3 3 0 010 5.28zm-16.83-9.77L39.17 32l-13.29 7.13z"
+      ></path>
+    </svg>
+  )
+}
+
+export const DownArrowIcon = () => {
+  return(
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0"
+      y="0"
+      version="1.1"
+      viewBox="0 0 29 29"
+      xmlSpace="preserve"
+    >
+      <path
+        fill="none"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M20).5 11.5l-6 6-6-6"
+      ></path>
+    </svg>
+  )
+}
+
+export const UpArrowIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+      <path
+        style={{
+          lineHeight: "normal",
+          textIndent: "0",
+          textAlign: "start",
+          WebkitTextDecorationLine: "none",
+          textDecorationLine: "none",
+          WebkitTextDecorationStyle: "solid",
+          textDecorationStyle: "solid",
+          WebkitTextDecorationColor: "#000",
+          textDecorationColor: "#000",
+          textTransform: "none",
+          isolation: "auto",
+          mixBlendMode: "normal",
+        }}
+        fill="#34485c"
+        fillRule="evenodd"
+        d="M3.7 11l-.7-.7L8.3 5l5.3 5.3-.7.7-4.6-4.6L3.7 11z"
+        color="#000"
+        fontFamily="sans-serif"
+        fontWeight="400"
+        overflow="visible"
+      ></path>
+    </svg>
+  )
+}
