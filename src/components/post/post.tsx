c1b353d6179c2051b4da3d715c3d78df86a11fe3
zerobug110 <mahabojang110@gmail.com>
2023-01-14 03:41:42 +0000

    added rounded shape icons

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -4,9 +4,20 @@ import './post.scss';
 export const Post = () => {
     return(
         <div className="post">
+            <div className="post__status">
+                {[0,1,2,3,4,5,6].map(item =>(
+                    <div className="post__status--item"></div>                    
+                ))
+
+                }
+
+            </div>
+            <Card>
+                <div className="post__upload"></div>
+            </Card>
             <div className="post__list">
                  <Card >
-                    <div className=""> profile</div>
+                    post 1
                 </Card>
             </div>
            
