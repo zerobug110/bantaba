3dcba8b6bba409bc2e3aa1c4d5754be868ff5c08
zerobug110 <mahabojang110@gmail.com>
2023-01-14 04:13:34 +0000

    added status and magnifying glass icon

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -208,3 +208,29 @@ export const UpArrowIcon = () => {
     </svg>
   )
 }
+
+export const MagnifyingGlassIcon = () => {
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256.001 256.001">
+      <path fill="none" d="M0 0H256V256H0z"></path>
+      <circle
+        cx="116"
+        cy="116"
+        r="84"
+        fill="none"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      ></circle>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+        d="M175.394 175.4L223.994 224.001"
+      ></path>
+    </svg>
+  )
+}
