6b8acf4952c830bfd09b271efbb6a8465f3d119a
zerobug110 <mahabojang110@gmail.com>
2023-01-17 01:13:44 +0000

    enhance ui

--- src/components/post/post-item.tsx
+++ src/components/post/post-item.tsx
@@ -6,11 +6,15 @@ export const PostItem = () => {
                     <div className="post-profile__info--image">
                         <img src="https://images.unsplash.com/photo-1664575599618-8f6bd76fc670?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" className="img" />
                     </div>
-                    <div className="post-profile__info--username">elan mosk</div>
+                    <div className="post-profile__info--details">
+                        <div className="username">
+                            elan mosk
+                        </div>
+                        <span className="post-timestamp">2 mins ago</span>
+                    </div>
                 </div>
                 <div className="post-profile__options">...</div>
             </div>
-            <div className="post-timestamp">2 mins ago</div>
             
             <div className="post-content">
                 <p className="post-content__description">
@@ -20,7 +24,23 @@ export const PostItem = () => {
                     <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" className="img" />
                 </div>
             </div>
-            
+
+            <div className="post-comment">
+                <div className="post-comment__reactions">
+                    <div className="reaction">like</div>
+                    <div className="reaction">like</div>
+                    <div className="reaction">like</div>
+                </div>
+                <div className="post-comment__profile">
+                    <div className="post-comment__profile--image">
+                        <img src="https://images.unsplash.com/photo-1664575599618-8f6bd76fc670?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" className="img" />
+                    </div>
+                    
+                    <div className="post-comment__input">
+                        <input type="text" name="" id="" placeholder="Write a Comment" className="input"/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
