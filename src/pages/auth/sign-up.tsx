0532cae6000d7d9073fb19350ad988f588fb0065
zerobug110 <mahabojang110@gmail.com>
2023-01-21 03:06:30 +0000

    added sign up inputs

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -2,6 +2,20 @@
 // import {db} from '../../utils/firebase/firebase.config'
 export const SignUp = () => {
     return(
-        <h1>sign up</h1>
+        <div className="sign-up">
+            <h3>sign up</h3>
+            <form action="" className="sign-up__forn">
+                <div className="sign-up__form--input">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="" className="input"/>
+                </div>
+                <div className="sign-up__form--input">
+                    <label htmlFor="password">password</label>
+                    <input type="text" name="password" id="" className="input"/>
+                </div>
+
+                <button className="sign-up__submit-btn">submit</button>
+            </form>
+        </div>
     )
 }
