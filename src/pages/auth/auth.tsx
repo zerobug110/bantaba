0b4036136ca9dbfd620908f08d4425c2f1ac7b5d
zerobug110 <mahabojang110@gmail.com>
2023-01-22 01:30:08 +0000

    fixed the react view slider by declaring it in react-view-slider.d.ts

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,12 +1,66 @@
+import { useState } from 'react';
 import './auth.scss'
 import { SignIn } from './sign-in'
 import { SignUp } from "./sign-up"
 
+import ViewSlider from 'react-view-slider'
+import { CloseIcon } from '../../assets/icons/icons';
+
 export const Auth = () => {
+    enum STEPS {
+        UPLOAD_IMAGE = 0,
+        SHARE_POST = 1,
+    }
+    const [step, setStep] = useState(0);
+
+    const VIEWS = [
+		({ index, active, transitionState }:any) => (
+			<section
+				className="first-slide slider"
+				hidden={index !== STEPS.UPLOAD_IMAGE}
+			>
+				<div>
+					<div className="slider-header">
+							<span className="app-icon">
+								{/* <CloseIcon></CloseIcon> */}
+							</span>
+						<button className="slider-movement" onClick={() => setStep(1)}>
+							Next
+						</button>
+
+					</div>
+				</div>
+                <SignUp />
+			</section>
+		),
+		({ index, active, transitionState }:any) => (
+			<section
+				className="secound-slide slider"
+				hidden={index !== STEPS.SHARE_POST}
+			>
+				<div className="slider-header">
+					<button onClick={() => setStep(0)}>
+						<span className="app-icon">
+							{/* <BackIcon></BackIcon> */}
+                            back
+						</span>
+					</button>
+				</div>
+
+            <SignIn />
+			</section>
+		),
+	];
+
+	const viewToRender = VIEWS[step];
+
     return (
         <div className="auth">
-            <SignUp />
-            <SignIn />
+            <ViewSlider
+					renderView={viewToRender}
+					numViews={2}
+					activeView={step}
+				></ViewSlider>
         </div>
     )
 }
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -5,19 +5,66 @@ import { useState } from "react"
 import { AppleIcon, GoogleIcon, MailIcon } from "../../assets/icons/icons"
 
 
-// import ViewSlider from 'react-view-slider'
+import ViewSlider from 'react-view-slider'
 
 export const SignUp = () => {
-    // enum STEPS {
-    //     UPLOAD_IMAGE = 0,
-    //     SHARE_POST = 1,
-    // }
+    enum STEPS {
+        UPLOAD_IMAGE = 0,
+        SHARE_POST = 1,
+    }
+    const [step, setStep] = useState(0);
     const [showPassword, setShowPassword] = useState('')
     const handleShowPassword = () => {
         setShowPassword((prevState):string | any => !prevState)
     }
+
+    const VIEWS = [
+		({ index, active, transitionState }:any) => (
+			<section
+				className="first-slide slider"
+				hidden={index !== STEPS.UPLOAD_IMAGE}
+			>
+				<div>
+					<div className="slider-header">
+						{/* <button onClick={() => handleClose()}> */}
+							<span className="app-icon">
+								{/* <CloseIcon></CloseIcon> */}
+							</span>
+						{/* </button> */}
+						<button className="slider-movement" onClick={() => setStep(1)}>
+							Next
+						</button>
+					</div>
+					{/* <PostMediaForm user={app.user}></PostMediaForm> */}
+				</div>
+			</section>
+		),
+		({ index, active, transitionState }:any) => (
+			<section
+				className="secound-slide slider"
+				hidden={index !== STEPS.SHARE_POST}
+			>
+				<div className="slider-header">
+					<button onClick={() => setStep(0)}>
+						<span className="app-icon">
+							{/* <BackIcon></BackIcon> */}
+						</span>
+					</button>
+				</div>
+			</section>
+		),
+	];
+
+	const viewToRender = VIEWS[step];
+
     return(
         <div className="sign-up">
+            <ViewSlider
+					// renderView={viewToRender}
+					numViews={2}
+					// activeView={step}
+				></ViewSlider>
+
             <form action="" className="sign-up__form">
             <div className="sign-up__form--input">
                     <input type="text" name="username" id="" className="input" placeholder="name"/>
