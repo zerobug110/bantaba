ae1dd1fb96bc88d50d012d13618fe138401ff61b
zerobug110 <mahabojang110@gmail.com>
2023-01-26 03:17:44 +0000

    added lazy route

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,11 +1,19 @@
 import {  Route, Routes  } from "react-router-dom"
 import {Nav} from '../nav/nav';
 import '../../index.css'
-import { Chat } from "../../pages/chat/Chat";
-import { Home } from "../../pages/home/home";
+// import { Chat } from "../../pages/chat/Chat";
+// import { Home } from "../../pages/home/home";
 import { Notifications } from "../../pages/notifications/notifications";
 import { Auth } from "../../pages/auth/auth";
+import { lazy } from "react";
 // import '../../App.scss';
+
+
+// @type-ignore
+// const Home = lazy(()=> import ('../../pages/home/home'))
+const Home = lazy(() => import('../../pages/home/home').then(({ Home }) => ({ default: Home })))
+
+const Chat = lazy(() => import('../../pages/chat/Chat').then(({ Chat }) => ({ default: Chat })))
 
 const AppRoutes = () => {
         return(
