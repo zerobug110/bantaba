ea8aa0a2cd58d8a22317b66e259a37e4e9e001a0
zerobug110 <mahabojang110@gmail.com>
2023-03-30 02:19:18 +0000

    enhance ivescream UI

--- frontend/src/pages/live-stream/live-stream.tsx
+++ frontend/src/pages/live-stream/live-stream.tsx
@@ -1,10 +1,27 @@
+import React,{ FC } from 'react';
+import { ChatIcon, LikeIcon } from '../../assets/icons/icons';
 import './live-stream.scss';
 
-export const LiveStream = () => {
+export const LiveStream:FC  = () => {
     return(
         <div className="live">
             <div className="live__video">
-                <h1>LiveStream</h1>
+                
+                {/* <h1>LiveStream</h1> */}
+                <div className="live__video--frame">
+                    <img src="" alt="" className="img" />
+                </div>
+                <div className="live__video--reaction">
+                    <div className="icon">
+                        <LikeIcon/>
+                    </div>
+                    <div className="icon">
+                        <ChatIcon/>
+                    </div>
+                    <div className="icon">
+                    <ChatIcon/>
+                    </div>
+                </div>
             </div>
         </div>
     )
