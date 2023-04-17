2c50b9fc9dffae5f374a2bcb80ead13b9cd443fc
zerobug110 <mahabojang110@gmail.com>
2023-04-17 01:33:20 +0000

    added all the models in the backend

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("expres")
+
+const app = express()
+
+const port = 2020;
+
+server.listen(port, "server started")
+
+app.
+app.get("/", (req, res) => {
+    res.send("hello app")
+})
+
