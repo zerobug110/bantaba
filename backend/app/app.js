0983e4117f2f3428538200dff8ebf29063fdf7ee
zerobug110 <mahabojang110@gmail.com>
2023-05-02 05:04:46 +0000

    fixed the development cach oveloading

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,5 +1,78 @@
 const express = require("express")
+const morgan = require("morgan")
 
 const app = express()
+app.use(morgan("dev"))
 
-module.export = app
+//Routes
+app.get('/api/v1/user/register', (res, req) => {
+    try {
+        res.status(200).json({
+            "status": "success",
+            "data": "user has been register"
+        })
+    } catch (error) {
+        res.json({
+            "status": "failed",
+            error: error.message
+        })
+    }
+})
+
+app.get('/api/v1/user/login', (res, req) => {
+    try {
+        res.status(200).json({
+            "status": "success",
+            "data": "user has been login"
+        })
+    } catch (error) {
+        res.json({
+            "status": "failed",
+            error: error.message
+        })
+    }
+})
+
+app.get('/api/v1/user/post', (res, req) => {
+    try {
+        res.status(200).json({
+            "status": "success",
+            "data": "user has post"
+        })
+    } catch (error) {
+        res.json({
+            "status": "failed",
+            error: error.message
+        })
+    }
+})
+
+app.get('/api/v1/user/comment', (res, req) => {
+    try {
+        res.status(200).json({
+            "status": "success",
+            "data": "user has commented"
+        })
+    } catch (error) {
+        res.json({
+            "status": "failed",
+            error: error.message
+        })
+    }
+})
+
+app.get('/api/v1/user/message', (res, req) => {
+    try {
+        res.status(200).json({
+            "status": "success",
+            "data": "user has sent a message"
+        })
+    } catch (error) {
+        res.json({
+            "status": "failed",
+            error: error.message
+        })
+    }
+})
+
+module.exports = app
